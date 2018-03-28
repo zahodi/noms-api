db6ffacd21e25339765dfc5e820fb8bab3d7ae63
Shaun Smiley <senorsmile@gmail.com>
2018-03-28 09:49:39 -0700

    remove unneeded cd

--- tests/tests.sh
+++ tests/tests.sh
@@ -4,14 +4,10 @@ cd "${BATS_TEST_DIRNAME}/"
 cd ../
 
 @test "Verify example_bin.sh returns 'it works'" {
-  #cd "${BATS_TEST_DIRNAME}/"
-  #cd ../
-
   output=$(./bin/example_bin.sh)
   [ "$output" == 'it works' ]
 }
 
 @test "Test travis" {
-  
   ./bin/example_bin.sh 
 }
