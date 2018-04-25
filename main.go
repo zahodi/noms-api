199159c6a7480e61d493fa081718902190cb2206
Valentin Gurmeza <valentin.gurmeza@globaleagle.com>
2018-04-25 09:14:41 -0700

    fix indentation

--- main.go
+++ main.go
@@ -1,24 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"os"
+  "fmt"
+  "os"
 
-	"github.com/attic-labs/noms/go/spec"
+  "github.com/attic-labs/noms/go/spec"
   "github.com/zahodi/noms-api/cmd"
 
 )
 
 func main() {
-	sp, err := spec.ForDataset("http://localhost:8000::people")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Could not create dataset: %s\n", err)
-		return
-	}
-	defer sp.Close()
-
-	if _, ok := sp.GetDataset().MaybeHeadValue(); !ok {
-		fmt.Fprintf(os.Stdout, "head is empty\n")
-	}
-  cmd.Test()
+  sp, err := spec.ForDataset("http://localhost:8000::people")
+  if err != nil {
+    fmt.Fprintf(os.Stderr, "Could not create dataset: %s\n", err)
+    return
+  }
+  defer sp.Close()
+  if _, ok := sp.GetDataset().MaybeHeadValue(); !ok {
+    fmt.Fprintf(os.Stdout, "head is empty\n")
+  }
+    cmd.Test()
 }
