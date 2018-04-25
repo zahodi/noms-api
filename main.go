769219473c57773c807eaffae2eda2d1e830fa2a
Valentin Gurmeza <valentin.gurmeza@globaleagle.com>
2018-04-24 23:14:36 -0700

    ignore noms-api

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/attic-labs/noms/go/spec"
+)
+
 func main() {
-	println("hello")
+	sp, err := spec.ForDatabase("http://localhost:8000")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Could not access database: %s\n", err)
+		return
+	}
+	defer sp.Close()
 }
